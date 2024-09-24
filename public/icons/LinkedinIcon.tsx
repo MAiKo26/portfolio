5ff0ffb60c3855c23977aa13b2ac5d088fa5fb84
Mohamed Aziz Karoui <medazizkaroui26@gmail.com>
2024-09-24 14:11:58 +0100

    fixed component issues

--- public/icons/LinkedinIcon.tsx
+++ public/icons/LinkedinIcon.tsx
@@ -11,9 +11,9 @@ export const LinkedinIcon = ({className, width, height}: Props) => (
     viewBox="0 0 24 24"
     fill="none"
     stroke="currentColor"
-    stroke-width="2"
-    stroke-linecap="round"
-    stroke-linejoin="round"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
     className="lucide lucide-linkedin"
   >
     <path
--- public/icons/MailIcon.tsx
+++ public/icons/MailIcon.tsx
@@ -11,9 +11,9 @@ export const MailIcon = ({className, width, height}: Props) => (
     viewBox="0 0 24 24"
     fill="none"
     stroke="currentColor"
-    stroke-width="2"
-    stroke-linecap="round"
-    stroke-linejoin="round"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
     className="lucide lucide-mail"
   >
     <rect
