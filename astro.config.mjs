07f229690a056514896e3b73baa876a9d7873bc5
Mohamed Aziz Karoui <medazizkaroui26@gmail.com>
2024-10-03 21:17:51 +0100

    reverted base to / from portfolio

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://maiko26.github.io",
-  base: "portfolio",
+  base: "/",
   integrations: [
     react(),
     tailwind({
