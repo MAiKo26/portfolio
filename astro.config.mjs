fc0641d746f4366a10384583b5562bed83b723f6
Mohamed Aziz Karoui <medazizkaroui26@gmail.com>
2024-10-18 21:02:41 +0100

    added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,18 @@
 import react from "@astrojs/react";
 import {defineConfig} from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   base: "/",
+  site: "https://portfolio.maiko26.tn",
   integrations: [
     react(),
     tailwind({
       applyBaseStyles: false,
     }),
+    sitemap(),
   ],
 });
